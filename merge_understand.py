3a3582039cc20ebfb27f86bb4337d16de4813a86
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-11-01 15:34:06 +0200

    add: merged package understand metrics into a csv

---
+++ merge_understand.py
@@ -0,0 +1,25 @@
+import os
+
+import pandas as pd
+
+
+all_dfs = []
+for folder in os.listdir(os.getcwd()):
+    if not os.path.isdir(folder):
+        continue
+    if not folder.endswith("-und"):
+        continue
+    for file in os.listdir(folder):
+        if not file.endswith("csv"):
+            continue
+        metrics_path = os.path.join(folder, file)
+        df = pd.read_csv(metrics_path)
+        df = df[df["Kind"] == "Package"]
+        df["MS_system"] = file[:-4]
+        cols = ["MS_system"] + [col for col in df.columns if col != "MS_system"]
+        df = df[cols]
+        all_dfs.append(df)
+
+all_dfs = sorted(all_dfs, key=lambda d: d.iloc[0,0])
+df = pd.concat(all_dfs)
+df.to_csv("understand_metrics.csv", index=False, header=True)

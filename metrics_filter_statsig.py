06c16ff4c7bcf4b04c1c406106937a643d1d97a1
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-11-13 11:19:18 +0200

    add: filter statistically significant metrics by type

---
+++ metrics_filter_statsig.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df_metrics = pd.read_csv("metrics_statsig.csv")
+
+df = pd.read_csv("metrics_microservice.csv")
+centrality_cols = [col for col in df.columns if "centrality" in col]
+complexity_metrics = (["MS_system", "Microservice"] + centrality_cols
+                      + list(df_metrics[df_metrics["type"] == "complexity"]["metric"]))
+df_complexity = df[complexity_metrics]
+df_complexity.to_csv("metrics_complexity.csv", index=False, header=True)
+size_metrics = (["MS_system", "Microservice"] + centrality_cols
+                + list(df_metrics[df_metrics["type"] == "size"]["metric"]))
+df_size = df[size_metrics]
+df_size.to_csv("metrics_size.csv", index=False, header=True)
+techdebt_metrics = (["MS_system", "Microservice"] + centrality_cols
+                    + list(df_metrics[df_metrics["type"] == "techdebt"]["metric"]))
+df_techdebt = df[techdebt_metrics]
+df_techdebt.to_csv("metrics_techdebt.csv", index=False, header=True)
